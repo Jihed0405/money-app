287913a74037ac434447264bccc3973ddd528082
Jihed0405 <jihed0405@gmail.com>
2023-03-27 18:41:14 +0100

    fix form-add widget style

--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -5,7 +5,12 @@ import '../utils/constants.dart';
 import '../widget/custom_toggle.dart';
 
 const List<String> list = <String>['CatOne', 'Two', 'Three', 'Four'];
-const List<String> list2 = <String>['ReccurrenceDaily', 'weekly', 'monthly', 'yearly'];
+const List<String> list2 = <String>[
+  'ReccurrenceDaily',
+  'weekly',
+  'monthly',
+  'yearly'
+];
 const List<Widget> typeOfADD = <Widget>[
   Text('Income'),
   Text('Expenses'),
@@ -19,11 +24,10 @@ class AddWidget extends StatefulWidget {
 }
 
 class _AddWidgetState extends State<AddWidget> {
-  
   List<bool> isSelected = <bool>[true, false];
   bool vertical = false;
   late TextEditingController _controller;
-    late TextEditingController _controller2;
+  late TextEditingController _controller2;
   String dropdownValue = list.first;
   String dropdownValue2 = list2.first;
   @override
@@ -42,8 +46,10 @@ class _AddWidgetState extends State<AddWidget> {
 
   @override
   Widget build(BuildContext context) {
-   double defaultWidth = MediaQuery.of(context).size.width; 
-   double defaultHeight = MediaQuery.of(context).size.height;
+    double defaultWidth = MediaQuery.of(context).size.width;
+    double defaultHeight = MediaQuery.of(context).size.height;
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return SingleChildScrollView(
       child: SafeArea(
         child: Column(
@@ -106,42 +112,226 @@ class _AddWidgetState extends State<AddWidget> {
               ),
             ]),
             Container(
-          child: TextField(
-                  decoration: const InputDecoration(
-                  focusedBorder:  OutlineInputBorder(
-                  borderSide: BorderSide(
-                  color: Colors.white,
-                  width: 1.0
-                      )
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                width: defaultWidth,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("some filds"),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 7,
+                                offset: const Offset(1, 1),
+                                color: Colors.grey.withOpacity(0.2))
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Amount',
+                          prefixIcon: Icon(
+                            Icons.monetization_on,
+                            color: Colors.green[200],
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          border: InputBorder.none,
+                        ),
+                        keyboardType:
+                            defaultTargetPlatform == TargetPlatform.iOS
+                                ? const TextInputType.numberWithOptions(
+                                    decimal: true, signed: true)
+                                : const TextInputType.numberWithOptions(
+                                    decimal: true),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(
+                              RegExp(r'^(\d+)?\.?\d{0,2}'))
+                        ],
+                        controller: _controller,
+                        onSubmitted: (String value) async {},
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 1.0
-                      )
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 7,
+                                offset: const Offset(1, 1),
+                                color: Colors.grey.withOpacity(0.2))
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Note',
+                          prefixIcon: Icon(
+                            Icons.note_rounded,
+                            color: Colors.red[200],
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          border: InputBorder.none,
+                        ),
+                        keyboardType:
+                            defaultTargetPlatform == TargetPlatform.iOS
+                                ? const TextInputType.numberWithOptions(
+                                    decimal: true, signed: true)
+                                : const TextInputType.numberWithOptions(
+                                    decimal: true),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(
+                              RegExp(r'^(\d+)?\.?\d{0,2}'))
+                        ],
+                        controller: _controller,
+                        onSubmitted: (String value) async {},
+                      ),
                     ),
-                                border: InputBorder.none,
-                                hintText: 'Amount',
-                                                 ),
-                                                 keyboardType: defaultTargetPlatform == TargetPlatform.iOS
-                                  ? const TextInputType.numberWithOptions(
-                                      decimal: true, signed: true)
-                                  : const TextInputType.numberWithOptions(decimal: true),
-                                                 inputFormatters: [
-                                FilteringTextInputFormatter.allow(
-                                    RegExp(r'^(\d+)?\.?\d{0,2}'))
-                                                 ],
-                                                 controller: _controller,
-                                                 onSubmitted: (String value) async {},
-                                               ),
-            ),
-                
-             
-             Row(
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 7,
+                                offset: const Offset(1, 1),
+                                color: Colors.grey.withOpacity(0.2))
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                  color: Colors.white, width: 1.0)),
+                          border: InputBorder.none,
+                          hintText: 'Amount',
+                        ),
+                        keyboardType:
+                            defaultTargetPlatform == TargetPlatform.iOS
+                                ? const TextInputType.numberWithOptions(
+                                    decimal: true, signed: true)
+                                : const TextInputType.numberWithOptions(
+                                    decimal: true),
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(
+                              RegExp(r'^(\d+)?\.?\d{0,2}'))
+                        ],
+                        controller: _controller,
+                        onSubmitted: (String value) async {},
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: defaultWidth,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 10,
+                                spreadRadius: 7,
+                                offset: const Offset(1, 1),
+                                color: Colors.grey.withOpacity(0.2))
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(defaultSpacing / 2),
+                        child: DropdownButtonFormField<String>(
+                          value: dropdownValue,
+                          decoration: const InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0)),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0))),
+                          borderRadius: BorderRadius.circular(30),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.black),
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              dropdownValue = value!;
+                            });
+                          },
+                          items: list
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: defaultWidth,
+                      height: 50,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              stops: [
+                                0.1,
+                                0.15,
+                                0.4,
+                                0.8,
+                              ],
+                              colors: [
+                                Color(0xFF35a6e5),
+                                Color(0xFF42a0e8),
+                                Color(0xFFd676db),
+                                Color(0xFFf88568)
+                              ]),
+                          border: Border.all(
+                            //color: const Color.fromARGB(255, 18, 32, 47),
+                            color: Colors.transparent,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(defaultRadius),
+                          ),
+                        ),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent),
+                          onPressed: () {
+                            print("on tapped buton");
+                          },
+                          child: const Text('Add'),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            Row(
               children: [
-              Padding(
-                padding: const EdgeInsets.all(defaultSpacing),
-                child: DropdownButton<String>(
+                Padding(
+                  padding: const EdgeInsets.all(defaultSpacing),
+                  child: DropdownButton<String>(
                     value: dropdownValue,
                     icon: const Icon(Icons.arrow_downward),
                     elevation: 16,
@@ -163,33 +353,38 @@ class _AddWidgetState extends State<AddWidget> {
                       );
                     }).toList(),
                   ),
-              ),]
-  ,),  
-        Row(
+                ),
+              ],
+            ),
+            Row(
               children: [
-              Expanded(child: DropdownButton<String>(
-      value: dropdownValue2,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value2) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue2 = value2!;
-        });
-      },
-      items: list2.map<DropdownMenuItem<String>>((String value2) {
-        return DropdownMenuItem<String>(
-          value: value2,
-          child: Text(value2),
-        );
-      }).toList(),
-    ),),]
-  ,), Row(
+                Expanded(
+                  child: DropdownButton<String>(
+                    value: dropdownValue2,
+                    icon: const Icon(Icons.arrow_downward),
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String? value2) {
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue2 = value2!;
+                      });
+                    },
+                    items: list2.map<DropdownMenuItem<String>>((String value2) {
+                      return DropdownMenuItem<String>(
+                        value: value2,
+                        child: Text(value2),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            Row(
               children: [
                 Expanded(
                   child: TextField(
@@ -197,20 +392,15 @@ class _AddWidgetState extends State<AddWidget> {
                       border: InputBorder.none,
                       hintText: 'Note',
                     ),
-                 
                     controller: _controller2,
                     onSubmitted: (String value) async {},
                   ),
                 ),
               ],
             ),
-             ],
-            ),
-          
-            ),
-          
-        );
-      
-    
+          ],
+        ),
+      ),
+    );
   }
 }
